0841ccf6138cd840f6af56b66b6fce116c67f81f
Anu Shibin Joseph Raj <anushibin007@gmail.com>
2026-04-28 22:57:00 +0530

    ✨ Allow CORS all

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import logging
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.api.endpoints import router as api_router
 
 # Configure logging
@@ -15,6 +16,14 @@ app = FastAPI(
     openapi_url="/openapi.json",
     docs_url="/docs",
     redoc_url="/redoc",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
